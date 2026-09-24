ad4b5c08ccf15a2f07afea2d80350a150be0ac54
jeanalvarez4004 <jeanalvarez4004@users.noreply.github.com>
2026-09-24 17:21:00 -0500

    Bump PWA cache v3 por login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='prism-v2';
+const CACHE='prism-v3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/usmp-logo.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
